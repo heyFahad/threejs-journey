4765bb28ab3ac1f030ad92167de72291f01b0237
Fahad Javed <fahad.javed1@arbisoft.com>
2024-05-06 21:57:09 +0500

    Positioning an object in 3D space

--- chapter-01-basics/04-transform-objects/src/script.js
+++ chapter-01-basics/04-transform-objects/src/script.js
@@ -14,6 +14,15 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Position - a 3D vector in space of type Vector3
+// Position can either be set individually in a single dimension
+mesh.position.x = 0.7; // positive value moves to the right, negative to the left
+mesh.position.y = -0.6; // positive value moves up, negative moves down
+mesh.position.z = 1; // positive value moves towards the camera, negative moves away from the camera
+
+// Or all at once in all 3 dimensions (both have same effect)
+mesh.position.set(0.7, -0.6, 1);
+
 /**
  * Sizes
  */
@@ -36,4 +45,6 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+
+// we need to do any transformation before rendering the scene, otherwise it will not be applied
 renderer.render(scene, camera);
