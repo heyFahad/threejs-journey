72088bcda64066115ffe5be01dda8280508e0d73
Fahad Javed <fahad.javed1@arbisoft.com>
2024-05-07 13:51:50 +0500

    Learning about the Perspective Camera and all of its parameters

--- chapter-01-basics/06-cameras/src/script.js
+++ chapter-01-basics/06-cameras/src/script.js
@@ -20,7 +20,18 @@ const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1, 5, 5, 5), new THREE.M
 scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+/**
+ * Camera: PerspectiveCamera
+ * The most common projection mode used for rendering a 3D scene.
+ * This projection mode is designed to mimic the way the human eye sees
+ *
+ * Parameters:
+ * 1. Field of view (fov): The vertical field of view of the camera in degrees.
+ * 2. Aspect ratio: The aspect ratio of the camera's view (width / height).
+ * 3. Near clipping plane: The closest distance that the camera can see.
+ * 4. Far clipping plane: The farthest distance that the camera can see. We can keep this value reasonably low to improve performance by not rendering objects that are too far away from the camera.
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.x = 2;
 camera.position.y = 2;
 camera.position.z = 2;
