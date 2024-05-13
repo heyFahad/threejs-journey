aaec286e77162b5cea7f8272021562bd4ca93d03
Fahad Javed <fahad.javed1@arbisoft.com>
2024-05-13 13:05:38 +0500

    Created our own random geometry using the BufferGeometry class

--- chapter-01-basics/08-geometries/src/script.js
+++ chapter-01-basics/08-geometries/src/script.js
@@ -10,8 +10,51 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 3, 4);
+// Object - Let's create a custom geometry using the BufferGeometry class
+const geometry = new THREE.BufferGeometry();
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 3, 4);
+
+/**
+ * A geometry is a collection of vertices and faces that define the shape of an object.
+ * In Three.js, there are two main types of geometries:
+ * - Geometry: The original geometry class that was available in Three.js. It is now deprecated in favor of BufferGeometry.
+ * - BufferGeometry: The newer and more efficient geometry class that is recommended for most use cases.
+ * The BufferGeometry class is more efficient because it stores the vertex data in a single buffer, which is better for performance.
+ *
+ * Let's create a set of 3 vertices to define a triangle.
+ */
+// const positionsArray = new Float32Array([
+//     0,
+//     0,
+//     0, // Vertex 1
+//     0,
+//     1,
+//     0, // Vertex 2
+//     1,
+//     0,
+//     0, // Vertex 3
+// ]);
+
+/**
+ * The positionsArray is a Float32Array that contains the vertex data for the triangle.
+ * To create a geometry, we need to create a BufferAttribute using our positionsArray and set it as the position attribute of the geometry.
+ */
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+// geometry.setAttribute('position', positionsAttribute);
+
+/**
+ * OR we can create a random geometry using the BufferGeometry class
+ */
+const count = 500; // number of triangles to create
+const positionsArray = new Float32Array(count * 3 * 3); // 3 vertices per triangle, 3 coordinates per vertex
+
+for (let i = 0; i < count * 3 * 3; i++) {
+    positionsArray[i] = (Math.random() - 0.5) * 2; // random number between -1 and 1
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute('position', positionsAttribute);
+
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
